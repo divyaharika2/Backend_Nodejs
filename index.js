4f806e67b4549be669c31dd5cef4e624075ae96c
Divya Harika <divyaharika24@gmail.com>
2025-07-24 20:08:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const dotEnv  = require('dotenv');
+const mongoose = require('mongoose');
+const vendorRoutes = require('./routes/vendorRoutes');
+const bodyParser = require('body-parser');
+const firmRoutes = require('./routes/firmRoutes');
+const productRoutes = require('./routes/productRoutes');
+const path = require('path');
+
+
+const app = express();
+const PORT = 4000;
+
+// Load environment variables
+dotEnv.config();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected successfully!"))
+  .catch((error) => console.log("MongoDB connection error:", error));
+
+app.use(bodyParser.json());
+app.use('/vendor', vendorRoutes);
+app.use('/firm', firmRoutes);
+app.use('/product', productRoutes);
+app.use('/uploads', express.static('uploads'));
+
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server started and running at port ${PORT}`);
+});
+
+
+// Define route
+app.use("/home", (req, res) => {
+  res.send("<h1>Welcome to Garam Godaari</h1>");
+});
+
+
