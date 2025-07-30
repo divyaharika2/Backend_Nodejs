12104aa0c62225130ecbc5bcfa008422d4c9dbf2
Divya Harika <divyaharika24@gmail.com>
2025-07-30 22:31:35 +0530

    updated in controllers

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,7 +7,7 @@ router.post('/add-product/:firmId', productController.addProduct);
 router.get('/:firmId/products', productController.getProductByFirm);
 router.get('/uploads/:imageName', (req, res) => {
     const imageName = req.params.imageName;
-    res.setHeader('Content-Type', 'image/jpeg');
+    res.header('Content-Type', 'image/jpeg');
     res.sendFile(path.join(__dirname, '..', 'uploads', imageName));
 });
 
