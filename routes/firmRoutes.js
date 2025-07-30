2c6cecc96a3be3b4f1f8e10615688aa890739737
Divya Harika <divyaharika24@gmail.com>
2025-07-30 23:52:02 +0530

    updated

--- routes/firmRoutes.js
+++ routes/firmRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router()
 router.post('/add-firm', verifyToken, firmController.addFirm);
 router.get('/uploads/:imageName', (req, res) => {
     const imageName = req.params.imageName;
-    res.headersSent('Content-Type', image/jpeg);
+    res.header('Content-Type', 'image/jpeg');
     res.sendFile(path.join(__dirname, '..', 'uploads', imageName));
 });
 
