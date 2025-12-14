25f8490dc7125b0a228017825cc663d130b009e9
Divya Harika <divyaharika24@gmail.com>
2025-12-14 21:42:49 +0530

    A descriptive message of your changes

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,33 +1,24 @@
-const Vendor = require('../models/Vendor');
 const jwt = require('jsonwebtoken');
 const dotEnv = require('dotenv');
 
-dotEnv.config()
+dotEnv.config();
+const secretKey = process.env.JWT_SECRET;
 
-const secretKey = process.env.WhatIsYourName
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // expects "Bearer <token>"
 
+  if (!token) {
+    return res.status(403).json({ error: "No token provided" });
+  }
 
-const verifyToken = async(req, res, next) => {
-    const token = req.headers.token;
-
-    if (!token) {
-        return res.status(401).json({ error: "Token is required" });
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: "Invalid token" });
     }
-    try {
-        const decoded = jwt.verify(token, secretKey)
-        const vendor = await Vendor.findById(decoded.vendorId).select('_id');
+    req.vendorId = decoded.vendorId;
+    next();
+  });
+};
 
-        if (!vendor) {
-            return res.status(404).json({ error: "vendor not found" })
-        }
-
-        req.vendorId = vendor._id
-
-        next()
-    } catch (error) {
-        console.error(error)
-return res.status(401).json({ error: "Invalid or expired token" });    }
-
-}
-
-module.exports = verifyToken
+module.exports = verifyToken;
